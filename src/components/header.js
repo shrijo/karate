583c94cd1030342dc040bead4fb5730b818de33a
shrijo <jonas.shriqui@gmail.com>
2019-09-19 16:40:14 +0200

    test

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,10 @@
 import React from "react"
-import Karate from "./karate"
+//import Karate from "./karate"
 //import styles from "./header.module.css"
 
 export default () => (
   <div /*className={styles.header}*/>
-    <Karate />
+    <h1>Karate</h1>
+    //<Karate />
   </div>
 )
