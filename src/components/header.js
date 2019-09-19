77346d6e2444f7f8acda4c6573d042c8123bcb3f
shrijo <jonas.shriqui@gmail.com>
2019-09-19 16:37:47 +0200

    no styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,9 @@
 import React from "react"
 import Karate from "./karate"
-import styles from "./header.module.css"
+//import styles from "./header.module.css"
 
 export default () => (
-  <div className={styles.header}>
+  <div /*className={styles.header}*/>
     <Karate />
   </div>
 )
