7540fabd288df5e50182dfeca88ec32e945cea42
shrijo <jonas.shriqui@gmail.com>
2019-09-19 15:36:13 +0200

    First Section

---
+++ src/components/fullpageCentered.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Logo from "./logo"
+import styles from "./fullpageCentered.module.css"
+
+export default () => (
+  <div className={styles.fullpage}>
+    <Logo headerText="Karate" />
+  </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,6 @@
 import React from "react"
+import FullpageCentered from "../components/fullpageCentered"
 
-export default () => <div>Hello Jonas!!!</div>
+export default () => (
+  <FullpageCentered />
+)
