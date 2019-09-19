3e7e055084c12142902e9c6ec594e44d256ea1d1
shrijo <jonas.shriqui@gmail.com>
2019-09-19 16:36:13 +0200

    no props

--- src/components/karate.js
+++ src/components/karate.js
@@ -1,6 +1,6 @@
 import React from "react"
-import styles from "./karate.module.css"
+//import styles from "./karate.module.css"
 
-export default props => (
+export default () => (
     <h1>Karate</h1>
 )
