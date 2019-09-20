718ffcbd4a4fcf9232952515fd3560f1bb08f4b5
shrijo <jonas.shriqui@gmail.com>
2019-09-20 11:34:39 +0200

    Scroll working

--- src/components/scrollerHorizontal.js
+++ src/components/scrollerHorizontal.js
@@ -1,20 +1,10 @@
 import React from 'react'
 import styles from "./scrollerHorizontal.module.css"
 
-//let posHorizontalScroller;
-
-/*function scrollingHorizontalScroller(){
-  alert("posHorizontalScroller");
-};*/
-
-function scrollingHorizontalScroller() {
-  //console.log(window.scrollY - posHorizontalScroller);
-};
-
 export default () => (
 
-<div className={styles.horizontalScroller} onWheel={scrollingHorizontalScroller}>
-  <div className={styles.horizontalSectionWrapper}>
+<div className="horizontalScroller">
+  <div className="horizontalSectionWrapper">
     <div className={styles.horizontalSection}>
       <div className={styles.card}>
         <h2>test</h2>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,22 @@ import Header from "../components/header"
 import ScrollerHorizontal from "../components/scrollerHorizontal"
 import Text from "../components/text"
 import Footer from "../components/footer"
+import styles from "./index.module.css"
 
+function scrollin(){
+  let posHorizontalScroller;
+  let posHorizontalSectionWrapper;
 
+  posHorizontalScroller = document.querySelector(".horizontalScroller").offsetTop
+  posHorizontalSectionWrapper = document.querySelector(".horizontalSectionWrapper").offsetTop
+
+  if (posHorizontalSectionWrapper > posHorizontalScroller) {
+    document.querySelector('.horizontalSectionWrapper').scrollTo((posHorizontalSectionWrapper - posHorizontalScroller), 0);
+  }
+}
 
 export default () => (
-
-  <div>
+  <div className={styles.wrapper} onScroll={scrollin}>
     <Header />
     <ScrollerHorizontal />
     <Text />
