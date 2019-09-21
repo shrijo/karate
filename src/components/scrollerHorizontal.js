3da07c36851dbecba808f3547f98604880969a58
Alex Jenter <jenteralex@gmail.com>
2019-09-21 18:30:50 +0200

    Fuck up hot mess

--- src/components/scrollerHorizontal.js
+++ src/components/scrollerHorizontal.js
@@ -1,33 +1,56 @@
-import React, { Component } from 'react'
+import React, { useRef,  useEffect } from 'react'
 import styles from "./scrollerHorizontal.module.css"
 
-let horizontalSectionWrapper = document.querySelector(".horizontalSectionWrapper");
 
 
 window.onresize = resize
 
 function resize(){
-  document.querySelector(".horizontalScroller").style.height = document.querySelector(".horizontalSectionWrapper").offsetWidth;
+  // document.querySelector(".horizontalScroller").style.height = this.wrapperRef.current.offsetWidth;
 }
 
 
-class ScrollerHorizontal extends Component {
-  componentDidMount() {
-    document.querySelector(".horizontalSectionWrapper").style.width = (document.querySelector(".horizontalSectionWrapper").childElementCount) * window.innerWidth +"px";
-    document.querySelector(".horizontalScroller").style.height  = (document.querySelector(".horizontalSectionWrapper").childElementCount - 1) * window.innerWidth + window.innerHeight +"px";
-    console.log('OHAI', this.props);
-    //horizontalSectionWrapper.css("background-color", "yellow");
-  }
 
-  render() {
+
+export default props => {
+    const scrollerRef = useRef(null);
+    const wrapperRef = useRef(null);
+    
+    let targetWidth;
+
+    const scrollHandler = () => {
+      const top = window.scrollY - wrapperRef.current.offsetTop;
+      const left = -1 * top;
+      const offsetTop = wrapperRef.current.offsetTop;
+      // const offsetBottom = wrapperRef.current.getBoundingClientRect().left + window.innerWidth;
+      
+      if(window.scrollY >= offsetTop ) {
+        console.log('OHAI', {
+          left: -1 * wrapperRef.current.getBoundingClientRect().left + window.innerWidth,
+          width: wrapperRef.current.getBoundingClientRect().width
+        });
+        
+        wrapperRef.current.style.transform = `translate(${left}px, ${top}px)`
+      }
+    }
+
+    useEffect(() => {
+      targetWidth = (wrapperRef.current.getBoundingClientRect().width) - window.innerWidth + window.innerHeight + `px`;
+      console.log(targetWidth)
+      scrollerRef.current.style.height = targetWidth
+      wrapperRef.current.style.transform = `translate(${100}, ${100})`
+      window.addEventListener('scroll', scrollHandler)
+      return  () => {
+        window.removeEventListener('scroll', scrollHandler)
+      }
+    })
+
     return (
-    <div className="horizontalScroller">
-      <div className="horizontalSectionWrapper">
-        { this.props.children }
+      <div className={styles.horizontalScroller} ref={scrollerRef}>
+        <div className={styles.horizontalSectionWrapper} ref={wrapperRef}>
+          { props.children }
+        </div>
       </div>
-    </div>
     )
-  }
 }
 
-export default ScrollerHorizontal
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,27 +9,27 @@ import Card  from "../components/Card"
 
 
 
-function scrollin(){
+// function scrollin(){
 
-  let posHorizontalScroller;
-  let posHorizontalSectionWrapper;
+//   let posHorizontalScroller;
+//   let posHorizontalSectionWrapper;
 
-  posHorizontalScroller = document.querySelector(".horizontalScroller").offsetTop
-  posHorizontalSectionWrapper = document.querySelector(".horizontalSectionWrapper").offsetTop
+//   posHorizontalScroller = document.querySelector(".horizontalScroller").offsetTop
+//   posHorizontalSectionWrapper = document.querySelector(".horizontalSectionWrapper").offsetTop
 
-  if (posHorizontalSectionWrapper >= posHorizontalScroller) {
-    //alert("now")
-    console.log(posHorizontalSectionWrapper - posHorizontalScroller);
-    document.querySelector('.horizontalSectionWrapper').scrollTo((posHorizontalSectionWrapper - posHorizontalScroller), 0);
-  }
-  else{
-    document.querySelector('.horizontalSectionWrapper').scrollTo(0, 0);
-  }
-}
+//   if (posHorizontalSectionWrapper >= posHorizontalScroller) {
+//     //alert("now")
+//     console.log(posHorizontalSectionWrapper - posHorizontalScroller);
+//     document.querySelector('.horizontalSectionWrapper').scrollTo((posHorizontalSectionWrapper - posHorizontalScroller), 0);
+//   }
+//   else{
+//     document.querySelector('.horizontalSectionWrapper').scrollTo(0, 0);
+//   }
+// }
 
 
 export default () => (
-  <div className={styles.wrapper} onScroll={scrollin}>
+  <div>
     <Header />
     <ScrollerHorizontal>
       <Card>
