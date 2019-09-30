efdd8ff52e122e1b015a198f948f86a391e9c71c
shrijo <jonas.shriqui@gmail.com>
2019-09-30 12:34:23 +0200

    adds

--- src/components/WorkedWith/index.js
+++ src/components/WorkedWith/index.js
@@ -5,6 +5,7 @@ import styles from "./WorkedWith.module.css"
 
 export default () => (
   <div className={styles.workedWithSection}>
+
     <ul className={styles.filterBar}>
       <li>All</li>
       <li>First</li>
@@ -12,5 +13,28 @@ export default () => (
       <li>Third</li>
       <li>Last Filter</li>
     </ul>
+
+    <ul className={styles.references}>
+      <li class="one">Reference</li>
+      <li class="four">Reference</li>
+      <li class="one">Reference</li>
+      <li class="three" className={styles.inactive}>Reference</li>
+      <li class="five">Reference</li>
+      <li class="one">Reference</li>
+      <li class="two">Reference</li>
+      <li class="two">Reference</li>
+      <li class="four">Reference</li>
+      <li class="three">Reference</li>
+      <li class="four">Reference</li>
+      <li class="one">Reference</li>
+      <li class="three">Reference</li>
+      <li class="two">Reference</li>
+      <li class="three">Reference</li>
+      <li class="one">Reference</li>
+      <li class="four">Reference</li>
+      <li class="three">Reference</li>
+      <li class="four">Reference</li>
+    </ul>
+    
   </div>
 )
